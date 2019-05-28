31a787d1c22b04ca759e461785437997cca396c4
meiji.heisei <curironakaka@yahoo.co.jp>
2019-05-28 05:19:47 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
  post 'login', to: 'sessions#create'
  delete 'logout', to: 'sessions#destroy'
  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-   resources :microposts, only: [:create, :destroy]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
